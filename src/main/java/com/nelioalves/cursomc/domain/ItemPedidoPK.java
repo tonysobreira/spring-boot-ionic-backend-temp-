56909d672e0e530f1add4de9320e61f76ab5ebe0
Tony <tonysobreira@gmail.com>
2019-10-09 09:58:58 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedidoPK.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedidoPK.java
@@ -9,7 +9,7 @@ import javax.persistence.ManyToOne;
 @Embeddable
 public class ItemPedidoPK implements Serializable {
 	private static final long serialVersionUID = 1L;
-
+	
 	@ManyToOne
 	@JoinColumn(name = "pedido_id")
 	private Pedido pedido;
